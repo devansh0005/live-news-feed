d3260037edc30f9fce378ba9dd3110cf11616710
Devansh Mittal <devanshmittal735@gmail.com>
2026-06-19 20:15:05 +0530

    chore: wrap fetch logic in try catch block

--- app.js
+++ app.js
@@ -2,16 +2,23 @@ const API_KEY = "YOUR_API_KEY_HERE";
 
 async function fetchNews(queryUrl) {
     const spinner = document.getElementById("loading-spinner");
-    if (spinner) {
-        spinner.style.display = "block";
+    try {
+        if (spinner) {
+            spinner.style.display = "block";
+        }
+
+        const response = await fetch(queryUrl);
+
+        if (spinner) {
+            spinner.style.display = "none";
+        }
+
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        if (spinner) {
+            spinner.style.display = "none";
+        }
+        console.error(error);
     }
-
-    const response = await fetch(queryUrl);
-
-    if (spinner) {
-        spinner.style.display = "none";
-    }
-
-    const data = await response.json();
-    console.log(data);
 }
